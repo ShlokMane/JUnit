835ee5bcb2bc350432ac871a9d0b4956ed54788c
ShlokMane <shlokm25@gmail.com>
2025-03-08 06:58:32 +0530

    Add Comments for Explaining the Logic

--- src/main/java/org/example/StringHelper.java
+++ src/main/java/org/example/StringHelper.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class StringHelper {
 
+    /**
+     * Removes 'A' characters from the first two positions of the given string.
+     * If the string length is 2 or less, it removes all occurrences of 'A'.
+     *
+     * @param str The input string.
+     * @return The modified string with 'A' removed from the first two positions.
+     */
     public String truncateAInFirst2Positions(String str) {
         if (str.length() <= 2)
             return str.replaceAll("A", "");
@@ -13,6 +20,15 @@ public class StringHelper {
                 + stringMinusFirst2Chars;
     }
 
+    /**
+     * Checks if the first two characters of the string are the same as the last two characters.
+     * - If the string length is 1 or less, returns false.
+     * - If the string length is exactly 2, returns true.
+     * - Otherwise, compares the first two and last two characters.
+     *
+     * @param str The input string.
+     * @return true if the first two characters match the last two, false otherwise.
+     */
     public boolean areFirstAndLastTwoCharactersTheSame(String str) {
 
         if (str.length() <= 1)
